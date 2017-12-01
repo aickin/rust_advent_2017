264611750ed0ddb29b291918a74fbec019aab875
Sasha Aickin <xander76@yahoo.com>
2017-11-30 22:10:40 -0800

    Added 2017 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod day1_2016;
+mod day1_2017;
 
 fn main() {
     println!("Let's do this!");
     println!("2016 day 1, puzzle 1: {}", day1_2016::go());
+    println!("2017 day 1, puzzle 1: {}", day1_2017::puzzle1());
+    println!("2017 day 1, puzzle 1: {}", day1_2017::puzzle2());
 }
