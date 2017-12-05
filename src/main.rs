5f650a628520f93877fafc9b06cf7eab1e7f97e0
Sasha Aickin <xander76@yahoo.com>
2017-12-04 22:01:39 -0800

    day 5

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     println!("Let's do this! Advent 2017!!!");
@@ -13,4 +14,6 @@ fn main() {
     println!("day 3, puzzle 2: {}", day3::puzzle2());
     println!("day 4, puzzle 1: {}", day4::puzzle1());
     println!("day 4, puzzle 2: {}", day4::puzzle2());
+    println!("day 5, puzzle 1: {}", day5::puzzle1());
+    println!("day 5, puzzle 2: {}", day5::puzzle2());
 }
